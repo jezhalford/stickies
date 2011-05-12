084b068570a9209ba560be19cf1c7ef339418a07
Jez Halford <jezhalford@protecinnovations.co.uk>
2011-05-12 17:27:25 +0100

    Added coordinate calculations

--- application/views/helpers/Labels.php
+++ application/views/helpers/Labels.php
@@ -10,6 +10,8 @@ class Zend_View_Helper_Labels extends Zend_View_Helper_Abstract {
     
     private $_columns;
     
+    private $_rows;
+    
     private $_padding;
     
     private $_fontSize;
@@ -33,10 +35,10 @@ class Zend_View_Helper_Labels extends Zend_View_Helper_Abstract {
         
         $this->_pdf->pages[] = $this->_page;
         
-        $this->_setCursorPositions();
-        
         $this->_page->setFont(Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA),
                 $this->_fontSize);
+        
+        $this->_setCursorPositions();
         
         $this->_drawLabels();
         
@@ -79,6 +81,13 @@ class Zend_View_Helper_Labels extends Zend_View_Helper_Abstract {
             throw new Zend_View_Exception('No valid column count specified');
         }
         
+        if(isset($spec['rows'])) {
+           $this->_rows = $spec['rows'];
+        }
+        else {
+            throw new Zend_View_Exception('No valid row count specified');
+        }
+        
         if(isset($spec['fontSize'])) {
            $this->_fontSize = $spec['fontSize'];
         }
@@ -118,7 +127,7 @@ class Zend_View_Helper_Labels extends Zend_View_Helper_Abstract {
         
         if(isset($spec['labels'])) {
             
-            $this->_labelSize = array('height' => 0, 'width' => 0);
+            $this->_labelSize = array('height' => 0, 'width' => 0, 'padding' => 0);
             
             foreach($spec['labels'] as $k => $v) {
                 
@@ -140,37 +149,76 @@ class Zend_View_Helper_Labels extends Zend_View_Helper_Abstract {
         
         $this->_cursors = array();
         
-        for($i = 0; $i < $this->_columns; $i++) {
+        $startingY = $this->_page->getHeight();
+        
+        $fontHeight = $this->_getFontHeight($this->_page->getFont(), $this->_page->getFontSize());
+        
+        for($j = 0; $j < $this->_rows; $j++) { // rows...
             
-            $x = 0;
+            $y = $startingY;
             
-            $y = 700;
-            
-            // we always need to add some left margin...
-            $x += $this->_margins['left'];
-            
-            if($i > 0) {
-                // if this isn't the first label in the row then we also need to add 
-                // the horizontal padding (i.e. the gap between columns) plus the width of the
-                // number of labels across we are...
-                $x += $this->_padding['horizontal'] * ($i+1);
-                
-                $x += $this->_labelSize['width'] * $i;
+            // we always need to add some top margin...
+            $y -= $this->_margins['top'];
+
+            if($j > 0) {
+                // if this isn't the first then we also need to add 
+                // the vertical padding (i.e. the gap between rows) plus the height of the
+                // number of labels down we have got...
+                $y -= $this->_padding['vertical'] * ($j);
+
+                $y -= ($this->_labelSize['height'] + $this->_labelSize['padding']) * $j;
                 
             }
             
-            $this->_cursors[] = array(
-                'x' => $x,
-                'y' => $y
-            );
+            // shunt downwards by the height of the font - coordinates for fonts 
+            // are set by their baseline.
+            $y -= $fontHeight;
+            
+            
+            for($i = 0; $i < $this->_columns; $i++) { // columns...
+
+                $x = 0;
+
+                // we always need to add some left margin...
+                $x += $this->_margins['left'];
+
+                if($i > 0) {
+                    // if this isn't the first label in the row then we also need to add 
+                    // the horizontal padding (i.e. the gap between columns) plus the width of the
+                    // number of labels across we have got...
+                    $x += $this->_padding['horizontal'] * ($i);
+
+                    $x += ($this->_labelSize['width'] + $this->_labelSize['padding']) * $i;
+
+                }
+
+                $this->_cursors[] = array(
+                    'x' => $x,
+                    'y' => $y
+                );
+            }
         }
-        
-//        echo '<pre>';
-//            print_r($this->_cursors);
-//        echo '</pre>';
         
     }
     
+    /**
+     * Find the height of a line of the font in points.
+     * 
+     * @param Zend_Pdf_Resource_Font $font
+     * @param float $fontSize
+     * @return float 
+     */
+    private function _getFontHeight($font, $fontSize) {
+        
+        return ($font->getLineHeight() / $font->getUnitsPerEm() * $fontSize);
+    }
+    
+    /**
+     * Convert millimeters to points.
+     * 
+     * @param type $mm
+     * @return type 
+     */
     private function _mmToPts($mm) {
         return $mm * 2.834645669;
     }
