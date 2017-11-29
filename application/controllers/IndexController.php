7a2fbc69ce8d5a70aae758ccaac328f35887bc24
Jez Halford <jeremy@theexchangelab.com>
2017-11-29 13:32:57 +0000

    Clean up whitespace

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -7,12 +7,11 @@ class IndexController extends Zend_Controller_Action
     {
         $spec = Zend_Json_Decoder::decode($this->_getParam('spec'));
         $data = Zend_Json_Decoder::decode($this->_getParam('data'));
-        
+
         $this->view->assign('spec', $spec);
         $this->view->assign('data', $data);
-        
+
     }
 
 
 }
-
--- application/views/helpers/Labels.php
+++ application/views/helpers/Labels.php
@@ -1,38 +1,38 @@
 <?php
 
 class Zend_View_Helper_Labels extends Zend_View_Helper_Abstract {
-    
+
     private $_pageSize;
-    
+
     private $_labelSize;
-    
+
     private $_margins;
-    
+
     private $_columns;
-    
+
     private $_rows;
-    
+
     private $_padding;
-    
+
     private $_fontSize;
-    
+
     private $_cursors;
-    
+
     /**
      * @var Zend_Pdf_Page
      */
     private $_page;
-    
+
     private $_pdf;
-    
+
     public function labels($spec, $data) {
-        
+
         $this->_parseSpec($spec);
-        
+
         $this->_pdf = new Zend_Pdf();
-        
+
         $labelsPerPage = $this->_rows * $this->_columns;
-        
+
         if(is_array($data)) {
             $paginatedData = array_chunk($data, $labelsPerPage);
                 foreach($paginatedData as $pageData) {
@@ -42,37 +42,37 @@ class Zend_View_Helper_Labels extends Zend_View_Helper_Abstract {
         else {
             $this->_renderPage($data);
         }
-        
-        
-        
+
+
+
         return $this->_pdf->render();
-        
+
     }
-    
+
     protected function _renderPage($data) {
         $this->_page = new Zend_Pdf_Page($this->_pageSize);
-        
+
         $this->_page->setFont(Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA),
                 $this->_fontSize);
-        
+
         $this->_setCursorPositions();
-        
+
         $this->_pdf->pages[] = $this->_page;
-        
+
         $this->_renderLabels($data);
-        
-        
+
+
     }
-    
+
     protected function _renderLabels($data) {
-        
+
         if(is_array($data)) {
-            
+
             foreach($data as $i => $datum) {
-                
+
                 $this->_writeLines($datum, $this->_cursors[$i]['x'], $this->_cursors[$i]['y']);
             }
-            
+
         }
         else {
             foreach($this->_cursors as $cursor) {
@@ -82,59 +82,59 @@ class Zend_View_Helper_Labels extends Zend_View_Helper_Abstract {
             }
         }
     }
-    
+
     protected function _writeLines($text, $x, $y) {
-        
+
         $lines = explode("\n", $text);
-        
+
         foreach($lines as $i => $line) {
             $this->_page->drawText($line, $x, $y - ($i *
                     $this->_getFontHeight($this->_page->getFont(), $this->_page->getFontSize())));
         }
-        
+
     }
-    
+
     /**
      * Sets up the view helper with info from the spec or suitable defaults.
-     * 
-     * @param array $spec 
+     *
+     * @param array $spec
      */
     private function _parseSpec($spec) {
-        
+
         if(isset($spec['pageSize']) && $spec['pageSize'] == 'A4') {
            $this->_pageSize = Zend_Pdf_Page::SIZE_A4;
         }
         else {
             throw new Zend_View_Exception('No valid page size specified');
         }
-        
+
         if(isset($spec['columns'])) {
            $this->_columns = $spec['columns'];
         }
         else {
             throw new Zend_View_Exception('No valid column count specified');
         }
-        
+
         if(isset($spec['rows'])) {
            $this->_rows = $spec['rows'];
         }
         else {
             throw new Zend_View_Exception('No valid row count specified');
         }
-        
+
         if(isset($spec['fontSize'])) {
            $this->_fontSize = $spec['fontSize'];
         }
         else {
             throw new Zend_View_Exception('No valid font size specified');
         }
-        
+
         if(isset($spec['margins'])) {
-            
+
             $this->_margins = array('top' => 0, 'bottom' => 0, 'left' => 0, 'right' => 0);
-            
+
             foreach($spec['margins'] as $k => $v) {
-                
+
                 if($k == 'top' || $k == 'bottom' || $k == 'left' || $k == 'right') {
                     $this->_margins[$k] = $this->_mmToPts($v);
                 }
@@ -143,11 +143,11 @@ class Zend_View_Helper_Labels extends Zend_View_Helper_Abstract {
         else {
             throw new Zend_View_Exception('No margins specified');
         }
-        
+
         if(isset($spec['padding'])) {
-            
+
             $this->_padding = array('horizontal' => 0, 'vertical' => 0);
-            
+
             foreach($spec['padding'] as $k => $v) {
                 if($k == 'horizontal' || $k == 'vertical') {
                     $this->_padding[$k] = $this->_mmToPts($v);
@@ -157,13 +157,13 @@ class Zend_View_Helper_Labels extends Zend_View_Helper_Abstract {
         else {
             throw new Zend_View_Exception('No padding specified');
         }
-        
+
         if(isset($spec['labels'])) {
-            
+
             $this->_labelSize = array('height' => 0, 'width' => 0, 'padding' => 0);
-            
+
             foreach($spec['labels'] as $k => $v) {
-                
+
                 if($k == 'height' || $k == 'width') {
                     $this->_labelSize[$k] = $this->_mmToPts($v);
                 }
@@ -172,41 +172,41 @@ class Zend_View_Helper_Labels extends Zend_View_Helper_Abstract {
         else {
             throw new Zend_View_Exception('No label size specified');
         }
-        
+
     }
-    
+
     /**
      * Populates the _cursors variable with starting x/y coordinates for each label on the page.
      */
     private function _setCursorPositions() {
-        
+
         $this->_cursors = array();
-        
+
         $startingY = $this->_page->getHeight();
-        
+
         $fontHeight = $this->_getFontHeight($this->_page->getFont(), $this->_page->getFontSize());
-        
+
         for($j = 0; $j < $this->_rows; $j++) { // rows...
-            
+
             $y = $startingY;
-            
+
             // we always need to add some top margin...
             $y -= $this->_margins['top'];
 
             if($j > 0) {
-                // if this isn't the first then we also need to add 
+                // if this isn't the first then we also need to add
                 // the vertical padding (i.e. the gap between rows) plus the height of the
                 // number of labels down we have got...
                 $y -= $this->_padding['vertical'] * ($j);
 
                 $y -= ($this->_labelSize['height'] + $this->_labelSize['padding']) * $j;
-                
+
             }
-            
-            // shunt downwards by the height of the font - coordinates for fonts 
+
+            // shunt downwards by the height of the font - coordinates for fonts
             // are set by their baseline.
             $y -= $fontHeight;
-            
+
             for($i = 0; $i < $this->_columns; $i++) { // columns...
 
                 $x = 0;
@@ -215,7 +215,7 @@ class Zend_View_Helper_Labels extends Zend_View_Helper_Abstract {
                 $x += $this->_margins['left'];
 
                 if($i > 0) {
-                    // if this isn't the first label in the row then we also need to add 
+                    // if this isn't the first label in the row then we also need to add
                     // the horizontal padding (i.e. the gap between columns) plus the width of the
                     // number of labels across we have got...
                     $x += $this->_padding['horizontal'] * ($i);
@@ -230,26 +230,26 @@ class Zend_View_Helper_Labels extends Zend_View_Helper_Abstract {
                 );
             }
         }
-        
+
     }
-    
+
     /**
      * Find the height of a line of the font in points.
-     * 
+     *
      * @param Zend_Pdf_Resource_Font $font
      * @param float $fontSize
-     * @return float 
+     * @return float
      */
     private function _getFontHeight($font, $fontSize) {
-        
+
         return ($font->getLineHeight() / $font->getUnitsPerEm() * $fontSize);
     }
-    
+
     /**
      * Convert millimeters to points.
-     * 
+     *
      * @param type $mm
-     * @return type 
+     * @return type
      */
     private function _mmToPts($mm) {
         return $mm * 2.834645669;
